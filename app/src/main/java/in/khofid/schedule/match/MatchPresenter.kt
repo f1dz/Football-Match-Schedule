86404d785f57a7380541f0cc38fdcdf1fcedd4e6
Khofidin <offiedz@gmail.com>
2018-10-24 23:04:36 +0700

    Change processBadge to Coroutines

--- app/src/main/java/in/khofid/schedule/match/MatchPresenter.kt
+++ app/src/main/java/in/khofid/schedule/match/MatchPresenter.kt
@@ -53,78 +53,77 @@ class MatchPresenter(
     }
 
     fun processBadge(ctx: Context, match: List<Match>) {
-        doAsync {
+        async(context.main) {
             match.forEach {
-                var homeTeams: List<Team> = listOf()
-                var awayTeams: List<Team> = listOf()
-                try {
-                    ctx.database.use {
-                        val result = select(Team.TABLE_TEAM)
-                            .whereArgs("(TEAM_ID = {id})", "id" to it.homeTeamId!!)
-                        homeTeams = result.parseList(classParser())
-                    }
-
-                    ctx.database.use {
-                        val result = select(Team.TABLE_TEAM)
-                            .whereArgs("(TEAM_ID = {id})", "id" to it.awayTeamId!!)
-                        awayTeams = result.parseList(classParser())
-                    }
-
-                } catch (e: SQLiteConstraintException) {
-                }
-
-                if (homeTeams.isEmpty()) {
-                    // Get data
-                    val response = Gson().fromJson(
-                        ApiRepository().doRequest(
-                            TheSportDBApi.getTeamDetail(it.homeTeamId!!)
-                        ),
-                        TeamResponse::class.java
-                    )
-
+                bg {
+                    var homeTeams: List<Team> = listOf()
+                    var awayTeams: List<Team> = listOf()
                     try {
                         ctx.database.use {
-                            insert(
-                                Team.TABLE_TEAM,
-                                Team.TEAM_ID to response.teams.first().idTeam,
-                                Team.TEAM_NAME to response.teams.first().strTeam,
-                                Team.TEAM_ALTERNATE to response.teams.first().strAlternate,
-                                Team.TEAM_BADGE to response.teams.first().strTeamBadge
-                            )
+                            val result = select(Team.TABLE_TEAM)
+                                .whereArgs("(TEAM_ID = {id})", "id" to it.homeTeamId!!)
+                            homeTeams = result.parseList(classParser())
                         }
-                    } catch (e: SQLiteConstraintException) {
-                        Log.d("LOG", e.localizedMessage)
-                    }
-                }
 
-                if (awayTeams.isEmpty()) {
-                    // Get data
-                    val response = Gson().fromJson(
-                        ApiRepository().doRequest(
-                            TheSportDBApi.getTeamDetail(it.homeTeamId!!)
-                        ),
-                        TeamResponse::class.java
-                    )
-
-                    try {
                         ctx.database.use {
-                            insert(
-                                Team.TABLE_TEAM,
-                                Team.TEAM_ID to response.teams.first().idTeam,
-                                Team.TEAM_NAME to response.teams.first().strTeam,
-                                Team.TEAM_ALTERNATE to response.teams.first().strAlternate,
-                                Team.TEAM_BADGE to response.teams.first().strTeamBadge
-                            )
+                            val result = select(Team.TABLE_TEAM)
+                                .whereArgs("(TEAM_ID = {id})", "id" to it.awayTeamId!!)
+                            awayTeams = result.parseList(classParser())
                         }
+
                     } catch (e: SQLiteConstraintException) {
-                        Log.d("LOG", e.localizedMessage)
+                    }
+
+                    if (homeTeams.isEmpty()) {
+                        // Get data
+                        val response = Gson().fromJson(
+                            ApiRepository().doRequest(
+                                TheSportDBApi.getTeamDetail(it.homeTeamId!!)
+                            ),
+                            TeamResponse::class.java
+                        )
+
+                        try {
+                            ctx.database.use {
+                                insert(
+                                    Team.TABLE_TEAM,
+                                    Team.TEAM_ID to response.teams.first().idTeam,
+                                    Team.TEAM_NAME to response.teams.first().strTeam,
+                                    Team.TEAM_ALTERNATE to response.teams.first().strAlternate,
+                                    Team.TEAM_BADGE to response.teams.first().strTeamBadge
+                                )
+                            }
+                        } catch (e: SQLiteConstraintException) {
+                            Log.e("ERROR", e.localizedMessage)
+                        }
+                    }
+
+                    if (awayTeams.isEmpty()) {
+                        // Get data
+                        val response = Gson().fromJson(
+                            ApiRepository().doRequest(
+                                TheSportDBApi.getTeamDetail(it.homeTeamId!!)
+                            ),
+                            TeamResponse::class.java
+                        )
+
+                        try {
+                            ctx.database.use {
+                                insert(
+                                    Team.TABLE_TEAM,
+                                    Team.TEAM_ID to response.teams.first().idTeam,
+                                    Team.TEAM_NAME to response.teams.first().strTeam,
+                                    Team.TEAM_ALTERNATE to response.teams.first().strAlternate,
+                                    Team.TEAM_BADGE to response.teams.first().strTeamBadge
+                                )
+                            }
+                        } catch (e: SQLiteConstraintException) {
+                            Log.d("LOG", e.localizedMessage)
+                        }
                     }
                 }
             }
-
-            uiThread {
                 view.processBadge()
-            }
         }
     }
 }
