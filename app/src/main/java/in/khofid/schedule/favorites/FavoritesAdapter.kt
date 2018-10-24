3c8a5175c791ffb974d623ffa9522251920da734
Khofidin <offiedz@gmail.com>
2018-10-24 13:16:55 +0700

    Add Utils Test and remove some unused code

--- app/src/main/java/in/khofid/schedule/favorites/FavoritesAdapter.kt
+++ app/src/main/java/in/khofid/schedule/favorites/FavoritesAdapter.kt
@@ -4,7 +4,6 @@ import `in`.khofid.schedule.R
 import `in`.khofid.schedule.db.Favorite
 import `in`.khofid.schedule.utils.toLocalDate
 import `in`.khofid.schedule.utils.toLocalTime
-import `in`.khofid.schedule.utils.toStringVoidNull
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -30,10 +29,10 @@ class FavoritesAdapter(private val ctx: Context, private var favorites: List<Fav
             itemView.match_date.text = fav.matchDate?.toLocalDate(fav.matchTime!!)
             itemView.match_time.text = fav.matchTime?.toLocalTime()
             itemView.home_team.text = fav.matchHomeTeam
-            itemView.home_score.text = fav.matchHomeScore?.toStringVoidNull()
+            itemView.home_score.text = fav.matchHomeScore?.toString()
             Picasso.get().load(fav.matchHomeBadge).into(itemView.home_team_badge)
             itemView.away_team.text = fav.matchAwayTeam
-            itemView.away_score.text = fav.matchAwayScore?.toStringVoidNull()
+            itemView.away_score.text = fav.matchAwayScore?.toString()
             Picasso.get().load(fav.matchAwayBadge).into(itemView.away_team_badge)
 
             itemView.setOnClickListener { listener(fav) }
