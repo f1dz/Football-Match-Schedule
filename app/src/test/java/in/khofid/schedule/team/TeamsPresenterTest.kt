ed07771d665a0aa9e29c3bef423026648ec590fd
Khofidin <offiedz@gmail.com>
2018-10-31 14:19:18 +0700

    Add Test Utils

---
+++ app/src/test/java/in/khofid/schedule/team/TeamsPresenterTest.kt
@@ -0,0 +1,21 @@
+package `in`.khofid.schedule.team
+
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class TeamsPresenterTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getTeamsList() {
+    }
+
+    @Test
+    fun fillLeagueSpinner() {
+    }
+}
