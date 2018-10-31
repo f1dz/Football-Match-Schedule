bd7c19cfb2f39292da151090c35aa6a65cc18ece
Khofidin <offiedz@gmail.com>
2018-10-31 15:01:53 +0700

    Add SearchMatchPresenterTest

---
+++ app/src/test/java/in/khofid/schedule/search/SearchMatchPresenterTest.kt
@@ -0,0 +1,56 @@
+package `in`.khofid.schedule.search
+
+import `in`.khofid.schedule.TestContextProvider
+import `in`.khofid.schedule.api.ApiRepository
+import `in`.khofid.schedule.api.TheSportDBApi
+import `in`.khofid.schedule.model.Match
+import com.google.gson.Gson
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+
+
+class SearchMatchPresenterTest {
+
+    @Mock private lateinit var view: SearchMatchView
+    @Mock private lateinit var gson: Gson
+    @Mock private lateinit var apiRepository: ApiRepository
+    @Mock private lateinit var matches: List<Match>
+
+    private lateinit var presenter: SearchMatchPresenter
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        presenter = SearchMatchPresenter(view, apiRepository, gson, TestContextProvider())
+    }
+
+    @Test
+    fun searchMatches() {
+        val query = "Juventus"
+        val response = SearchMatchResponse(matches)
+
+        `when`(
+            gson.fromJson(
+                apiRepository.doRequest(TheSportDBApi.searchEvents(query)),
+                SearchMatchResponse::class.java
+            )
+        ).thenReturn(response)
+
+        presenter.searchMatches(query)
+
+        verify(view).showLoading()
+        verify(view).showMatchesList(matches)
+        verify(view).hideLoading()
+
+    }
+
+    @Test
+    fun processBadge() {
+
+
+    }
+}
